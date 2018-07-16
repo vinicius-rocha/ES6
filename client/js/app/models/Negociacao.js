b3b7aab39a30e55e3bf72b3d05ff193aea8ad09f
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-16 18:28:34 -0300

    Cap2 - A Sintaxe get
    - utilizando a sintaxe get e a utilizando no index.

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -6,16 +6,16 @@ class Negociacao {
         this._valor = valor;
     }
 
-    getVolume(){
+    get volume(){
         return this._quantidade * this._valor;
     }
-    getData(){
+    get data(){
         return this._data;
     }
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade;
     }
-    getValor(){
+    get valor(){
         return this._valor;
     }
 }
