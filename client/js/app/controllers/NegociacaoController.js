3aa571b90ad103f63d29bf4c6e681b415f6e16e5
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-17 11:49:08 -0300

    Cap4 - ListModel
        - Criando a lista de negociacoes;

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,19 +7,30 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
                 
-        let negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
         
-        console.log(negociacao);
+        console.log(this._listaNegociacoes.negociacoes);
+    }
 
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _criaNegociacao() {
+        return new Negociacao(
+              DateHelper.textoParaData(this._inputData.value),
+              this._inputQuantidade.value,
+              this._inputValor.value
+          );
+    }          
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+        this._inputData.focus();
     }
 }
