ed34dcff575f3ed6f6e287285e14362209590dfd
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-19 16:06:13 -0300

    M2-Cap1 - Lembrou de Atualizar a view? Nem eu

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -36,6 +36,14 @@ class NegociacaoController{
               this._inputValor.value
           );
     }          
+    
+    apaga(){
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+    }
+
     _limpaFormulario(){
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
