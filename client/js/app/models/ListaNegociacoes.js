5dbed240297255a7bb5b4361aacbb89b2271352f
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-19 21:41:27 -0300

    M2Cap1 - API Reflection
    Utilizando reflection para criar uma armadilha a ser chamada sempre que
    adiconarmos ou removermos alguma negociação.

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,18 +1,22 @@
 class ListaNegociacoes{
 
-    constructor(){
+    constructor(contexto,armadilha){
         this._negociacoes = [];
+        this._armadilha = armadilha;
+        this._contexto = contexto;
     }
 
-    adiciona(negociacao){
-        this._negociacoes.push(negociacao);
-    }
-    
     get negociacoes(){
         return [].concat(this._negociacoes);
     }
 
+    adiciona(negociacao){
+        this._negociacoes.push(negociacao);
+        Reflect.apply(this._armadilha, this._contexto, [this]);
+    }
+    
     esvazia(){
         this._negociacoes = [];
+        Reflect.apply(this._armadilha, this._contexto, [this]);
     }
-}
+}this._listaNegociacoes
