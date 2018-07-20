cb3a1561d1b42fffb5293757ffa081db6c3559db
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-19 21:47:33 -0300

    M2Cap1 - Arrow Function e o escopo léxico
    Alterando o código anterior para fazer uso do escopo léxico que as
    arrows functions possuem.

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,9 +1,8 @@
 class ListaNegociacoes{
 
-    constructor(contexto,armadilha){
+    constructor(armadilha){
         this._negociacoes = [];
         this._armadilha = armadilha;
-        this._contexto = contexto;
     }
 
     get negociacoes(){
@@ -12,11 +11,11 @@ class ListaNegociacoes{
 
     adiciona(negociacao){
         this._negociacoes.push(negociacao);
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
     
     esvazia(){
         this._negociacoes = [];
-        Reflect.apply(this._armadilha, this._contexto, [this]);
+        this._armadilha(this);
     }
-}this._listaNegociacoes
+}
