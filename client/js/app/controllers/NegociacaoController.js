0eebd65a7e5b81cab56729e3792122d2668577d2
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-24 15:53:07 -0300

    M2Cap4 - Requisições Ajax
    -Preparando a estrutura para executar ajax e realizar importação de
    json.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -33,6 +33,21 @@ class NegociacaoController{
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
 
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    console.log('Obtendo as negociações do servidor.')
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor.')
+                }
+            }
+        };
+        xhr.send();
+    }
+
     _limpaFormulario(){
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
