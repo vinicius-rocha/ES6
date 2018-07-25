db282fcd649c6b619fb28e54458615f3f84e2174
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-25 14:43:33 -0300

    M3Cap2 - Criando stores
    - Utilizando a classe Connection Factory para realizar a criação das
    stores.

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -13,16 +13,26 @@ class ConnectionFactory {
             let openRequest = window.indexedDB.open(dbName, version);
 
             openRequest.onupgradeneeded = e => {
-
+                ConnectionFactory._createStores(e.target.result);
             };
 
             openRequest.onsuccess = e => {
-
+                resolve(e.target.result);
             };
 
             openRequest.onerror = e => {
-
+                console.log(e.target.error.name);
+                reject(e.target.error.name);
             };
+        });
+    }
+
+    static _createStores(connection){
+        stores.forEach(store => {
+            if(connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store);
+            
+            connection.createObjectStore(store, {autoincrement:true})
         });
     }
 }
