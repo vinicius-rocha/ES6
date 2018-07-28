5d35a892fd1ccf537af62996379fd0e87253d79f
vinicius-rocha <r.vinicius.09@gmail.com>
2018-07-28 12:54:59 -0300

    M3Cap4 - Usando o método some
    - Utilzando o some() para realizar a filtragem e evitar a importação
    duplicada.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -67,6 +67,13 @@ class NegociacaoController{
         let service = new NegociacaoService();
         service
             .obterNegociacoes()
+            .then(negociacoes =>
+                negociacoes.filter(negociacao =>
+                    !this._listaNegociacoes.negociacoes.some(negociacaoExistente =>
+                        JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)
+                    )
+                )
+            )
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.adiciona(negociacao);
                 this._mensagem.texto = 'Negociações importadas com sucesso';
